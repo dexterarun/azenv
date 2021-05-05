3342b367fc3286caa6951ee5c59ffcfc01085dd5
suren <suren>
2021-05-05 22:09:19 +0100

    added outputs for aks vm and k8s-iaas

---
+++ terraform/modules/k8s-iaas/outputs.tf
@@ -0,0 +1,15 @@
+output "master_vm" {
+    value = vm_master.vm
+}
+
+output "master_nic" {
+    value = vm_master.nic
+}
+
+output "worker_vm" {
+    value = vm_worker.vm
+}
+
+output "worker_nic" {
+    value = vm_worker.nic
+}
